b2f5dbacaf76943f93ba65858f6a23be8208795a
Moreno, Daniel <danielmoreno3@my.unt.edu>
2023-11-21 12:05:43 -0600

    Implement light and dark modes

--- app/src/main/java/com/example/androiduiexperiments/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/androiduiexperiments/ui/settings/SettingsFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat.recreate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.androiduiexperiments.databinding.FragmentSettingsBinding
+import com.example.androiduiexperiments.R
+import com.google.android.material.switchmaterial.SwitchMaterial
+import java.util.Objects
 
 class SettingsFragment : Fragment() {
 
@@ -33,6 +40,22 @@ class SettingsFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val modeSwitcherBtn = view.findViewById<SwitchMaterial>(R.id.switchBtn)
+        modeSwitcherBtn.setText("Light Mode")
+
+        modeSwitcherBtn?.setOnCheckedChangeListener({ _ , isChecked ->
+            if (isChecked) modeSwitcherBtn.setText("Night Mode")
+            else modeSwitcherBtn.setText("Light Mode")
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked) AppCompatDelegate.MODE_NIGHT_YES
+                else AppCompatDelegate.MODE_NIGHT_NO
+            )
+        })
     }
 
     override fun onDestroyView() {
